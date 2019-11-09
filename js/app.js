567d662baf0710331972ef8554f6b8e725d8abde
DuaLoad <asd101dsfp001sk@mail.ru>
2019-11-10 02:21:18 +0300

    Доработана навигации, её анимация и плавная прокрутка к ссылкам

--- js/app.js
+++ js/app.js
@@ -1,6 +1,37 @@
 const randomNum = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
 const r = (value) => value[Math.floor(Math.random() * value.length)];
 
+$(document).on('click','.menuToggle', function(e) {
+    e.preventDefault();
+
+    const
+        i = 'navOpened',
+        body = $('body'),
+        check = body.hasClass(i),
+        navOpen = () => $('#nav').fadeToggle(0),
+        sideBarOpen = () => body.toggleClass(i);
+
+    setTimeout(navOpen, (check) ? 150 : 0);
+    setTimeout(sideBarOpen, (check) ? 0 : 1);
+});
+
+$(document).on('click','a[href*="#"]', function(e) {
+    let link = $(this).attr('href');
+    if(link.length > 1 && $('*').is(`${link}`)){
+        e.preventDefault(e);
+        let id = $(`${link}`);
+
+        if($('body').hasClass('navOpened')) $('#nav .menuToggle').click();
+        $('html').animate({ scrollTop: id.offset().top }, 500);
+    }
+});
+
+/*
+#######################                  #######################
+### @ @ @ @ @ @ @ @ ### СО СТРИМАЧА КОД  ### @ @ @ @ @ @ @ @ ###
+#######################                  #######################
+*/
+
 $(document).on('click','.servers_list .server', function() {
     let i = $(this),
         c = 'checked';
